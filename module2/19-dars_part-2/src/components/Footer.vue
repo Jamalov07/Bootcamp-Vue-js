9de885bb8716605846323da57a8af21be141fa95
Jamalov <Jamalovn07@gmail.com>
2023-02-14 16:37:18 +0500

    add lesson 19 with Vue starter

---
+++ module2/19-dars_part-2/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <p>{{ new Date().getFullYear() }}</p>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style>
+footer {
+  width: 100%;
+  background-color: #000;
+  color: white;
+  padding: 10px;
+  height: 70px;
+  text-align: center;
+  font-size: 24px;
+}
+</style>
---
+++ module2/19-dars_part-2/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1 class="text-4xl bg-sky-600 border-yellow-600">Header</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  background-color: crimson;
+  color: white;
+  padding: 10px;
+}
+</style>
