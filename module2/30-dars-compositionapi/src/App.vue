7fc65f85d28b859d5d56f751cc22863cf7185917
Jamalov <Jamalovn07@gmail.com>
2023-03-02 11:01:57 +0500

    add lesson-30

---
+++ module2/30-dars-compositionapi/src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <div class="d-flex flex-wrap justify-content-around w-100"></div>
+  </div>
+</template>
+
+<style scoped></style>
